6a0d9101de17869837a57b6e811550cc5e18d077
Deep <diphiwase0987@gmail.com>
2026-03-16 15:06:17 +0530

    complete solution

--- app.js
+++ app.js
@@ -12,7 +12,58 @@ app.get("/", (req, res) => {
 });
 
 // solution starts
+app.get("/findColleges", async (req, res) => {
+  try {
+    let { name, state, city, minPackage, maxFees, course, exam } = req.query;
 
+    minPackage = Number(minPackage);
+    maxFees = Number(maxFees);
+
+    if ((minPackage && minPackage < 0) || (maxFees && maxFees < 0)) {
+      return res.status(400).json("Invalid Data like minPackage or maxFees");
+    }
+
+    let query = {};
+
+    if (name) {
+      query.name = { $regex: name, $options: "i" };
+    }
+
+    if (state) {
+      query.state = { $regex: state, $options: "i" };
+    }
+
+    if (city) {
+      query.city = { $regex: city, $options: "i" };
+    }
+
+    if (minPackage) {
+      query.minPackage = { $gte: minPackage };
+    }
+
+    if (maxFees) {
+      query.maxFees = { $lte: maxFees };
+    }
+
+    if (course) {
+      query.course = course;
+    }
+
+    if (exam) {
+      const exams = exam.split(",");
+      query.exam = { $in: exams };
+    }
+
+    const result = await collegeModel
+      .find(query, "name city state exam course maxFees minPackage")
+      .exec();
+
+    res.status(200).json(result);
+
+  } catch (err) {
+    res.status(500).json("Server Error");
+  }
+});
 // solution end
 
 module.exports = { app };
